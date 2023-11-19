f002b0e9c54ed2539dc2b3512ff23e4b67710c50
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-11-19 11:28:12 -0300

    Feat: create some methods and atributtes

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent {
 
+export class FormsComponent implements OnInit{
+  results = [];
+  
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder : FormBuilder) {
+
+  }
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      periodo : '',
+      unidadesFechadas : false
+    }) 
+  }
+
+    onSubmit() {
+      console.log(this.formGroup.value);
+    }
+    onClear() {
+      this.formGroup.reset();
+    }
 }
